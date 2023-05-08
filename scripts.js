a160696ae756c2159d766e2b08b4d800d0229c4a
Thai Le <tple06203@gmail.com>
2023-05-08 14:42:58 -0700

    Add menu selection and basic features.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 const container = document.getElementById("container");
+const clearButton = document.getElementById("clear-button");
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+clearButton.onClick = () => clearGrid();
+
+function clearGrid() {
+    makeGrids(16, 16);
+}
 
 function makeGrids(row, column)
 {
@@ -6,9 +17,16 @@ function makeGrids(row, column)
     container.style.setProperty("--grid-column", column);
     for (let i = 0; i < (row * column); i++)
     {
-        let grid = document.createElement("div");
+        const grid = document.createElement("div");
+        grid.addEventListener("mouseover", changeColor);
+        grid.addEventListener("mousedown", changeColor);
         container.appendChild(grid).className = "grid";
     };
 };
 
 makeGrids(16, 16);
+
+function changeColor(e) {
+    if (e.type === "mouseover" && !mouseDown) return;
+    e.target.style.backgroundColor = "black";
+}
