89a0ba32bf5e36805d1df7e2cbb2cfb523ee3d82
Thai Le <tple06203@gmail.com>
2023-05-13 12:28:44 -0700

    Add grid size change slider and fix bugs.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const DEFAULT_COLOR = "black";
 const DEFAULT_MODE = "single";
+const DEFAULT_SIZE = 16;
 
 let currentMode = DEFAULT_MODE;
 let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
 let mouseDown = false;
 
 
@@ -16,6 +18,8 @@ const singleBTN = document.getElementById("single-color-button");
 const rainbowBTN = document.getElementById("rainbow-color-button");
 const eraserBTN = document.getElementById("eraser-button");
 const clearBTN = document.getElementById("clear-button");
+const textInput = document.getElementById("textInput");
+const slider = document.getElementById("slider");
 
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
@@ -24,17 +28,38 @@ colorPicker.oninput = (e) => changeCurrentColor(e.target.value);
 singleBTN.onclick = () => changeCurrentMode("single");
 rainbowBTN.onclick = () => changeCurrentMode("rainbow");
 eraserBTN.onclick = () => changeCurrentMode("eraser");
+clearBTN.onclick = () => newGrid();
 
-function makeGrids(row, column)
+let update = () => textInput.innerHTML = `${slider.value} x ${slider.value}`;
+
+slider.addEventListener("input", update);
+update();
+
+slider.addEventListener("change", (event) => {
+    currentSize = slider.value;
+    newGrid();
+})
+
+function clearGrid() {
+    sketchpad.innerHTML = "";
+}
+
+function newGrid() {
+    clearGrid();
+    makeGrids(currentSize);
+}
+
+function makeGrids(row)
 {
     sketchpad.style.setProperty("--grid-row", row);
-    sketchpad.style.setProperty("--grid-column", column);
-    for (let i = 0; i < (row * column); i++)
+    sketchpad.style.setProperty("--grid-column", row);
+    for (let i = 0; i < (row * row); i++)
     {
         const grid = document.createElement("div");
+        grid.classList.add("grid");
         grid.addEventListener("mouseover", changeColor);
         grid.addEventListener("mousedown", changeColor);
-        sketchpad.appendChild(grid).className = "grid";
+        sketchpad.appendChild(grid);
     };
 };
 
@@ -58,31 +83,33 @@ function changeColor(e) {
 }
 
 function activeButton(newMode) {
-    if (currentMode === "rainbow")
-    {
-        rainbowBTN.classList.remove("active");
-    }
-    else if (currentMode === "single")
+
+    if (currentMode === "single")
     {
         singleBTN.classList.remove("active");
+    }
+    else if (currentMode === "rainbow")
+    {
+        rainbowBTN.classList.remove("active");
     }
     else if (currentMode === "eraser")
     {
         eraserBTN.classList.remove("active");
     }
 
-    if (newMode === "rainbow")
-    {
-        rainbowBTN.classList.add("active");
-    }
-    else if (newMode === "single")
+    if (newMode === "single")
     {
         singleBTN.classList.add("active");
+    }
+    else if (newMode === "rainbow")
+    {
+        rainbowBTN.classList.add("active");
     }
     else if (newMode === "eraser")
     {
         eraserBTN.classList.add("active");
     }
+
 }
 
 function changeCurrentMode(newMode) {
@@ -92,6 +119,6 @@ function changeCurrentMode(newMode) {
 
 
 window.onload = () => {
-    makeGrids(16, 16);
+    makeGrids(currentSize);
     activeButton(DEFAULT_MODE);
 }
